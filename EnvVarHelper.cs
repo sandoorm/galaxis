f435e56af0409b0395d1950fe0288dc61c4dfa75
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-22 14:10:26 +0200

    connection string modified

--- EnvVarHelper.cs
+++ EnvVarHelper.cs
@@ -4,6 +4,11 @@ namespace GalaxisProjectWebAPI
 {
     public static class EnvVarHelper
     {
-        public static string GetGalaxisDbConnectionString() => Environment.GetEnvironmentVariable("GALAXIS_CONNECTION_STRING_APP");
+        public static string GetGalaxisDbConnectionString() =>
+            "User Id=" + Environment.GetEnvironmentVariable("POSTGRESQL_USER")
+            + ";Password=" + Environment.GetEnvironmentVariable("POSTGRESQL_PASSWORD")
+            + ";Port=5432"
+            + ";Database=" + Environment.GetEnvironmentVariable("POSTGRESQL_DATABASE")
+            + ";";
     }
 }
