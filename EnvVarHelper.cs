8aa51943f8065e4e00eed4d2b0771413fbc29505
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-22 15:05:15 +0200

    with port with server

--- EnvVarHelper.cs
+++ EnvVarHelper.cs
@@ -4,10 +4,12 @@ namespace GalaxisProjectWebAPI
 {
     public static class EnvVarHelper
     {
-        // + ";Server=postgresql"
+        
         public static string GetGalaxisDbConnectionString() =>
             "User Id=" + Environment.GetEnvironmentVariable("POSTGRESQL_USER")
             + ";Password=" + Environment.GetEnvironmentVariable("POSTGRESQL_PASSWORD")
+            + ";Port=5432"
+            + ";Server=postgresql"
             + ";Database=" + Environment.GetEnvironmentVariable("POSTGRESQL_DATABASE")
             + ";";
     }
