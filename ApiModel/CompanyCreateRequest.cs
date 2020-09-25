461cebcbd4877ee5e15ba7bad5e036d8e59195d1
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-25 15:19:33 +0200

    Required annotations

--- ApiModel/CompanyCreateRequest.cs
+++ ApiModel/CompanyCreateRequest.cs
@@ -1,15 +1,34 @@
-﻿namespace GalaxisProjectWebAPI.ApiModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GalaxisProjectWebAPI.ApiModel
 {
     public class CompanyCreateRequest
     {
+        [Required]
         public string CompanyName { get; set; }
+
+        [Required]
         public string CompanyAddress { get; set; }
+
+        [Required]
         public string RegistrationNumber { get; set; }
+
+        [Required]
         public string vatNumber { get; set; }
+
+        [Required]
         public string OfficialEmailAddress { get; set; }
+
+        [Required]
         public string ContactPersonName { get; set; }
+
+        [Required]
         public string Position { get; set; }
+
+        [Required]
         public string ContactPersonPhoneNumber { get; set; }
+
+        [Required]
         public string ContactPersonEmail { get; set; }
     }
 }
