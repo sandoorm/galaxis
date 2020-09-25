1067b6e344059588b095199d61a840d3790f8e9e
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-25 12:49:09 +0200

    conn string

--- EnvVarHelper.cs
+++ EnvVarHelper.cs
@@ -2,8 +2,9 @@
 {
     public static class EnvVarHelper
     {
-        // "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.77.30;Port=5432;Database=sampledb1-galaxis;";
+        // "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.50.48;Port=5432;Database=sampledb2-galaxis;";
+        // "User Id=galaxis;Password=galaxis;Server=localhost;Port=5432;Database=galaxis;";
         public static string GetGalaxisDbConnectionString() =>
-            "User Id=galaxis;Password=galaxis;Server=localhost;Port=5432;Database=galaxis;";
+            "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.50.48;Port=5432;Database=sampledb2-galaxis;";
     }
 }
