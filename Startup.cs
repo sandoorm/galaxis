55db44160b0bbbba2eed332ca5a31dc280fb73df
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-25 12:58:32 +0200

    Cors fix

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace GalaxisProject_WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options => options.AddPolicy(name: MyAllowSpecificOrigins,
+            services.AddCors(options => options.AddPolicy(MyAllowSpecificOrigins,
                 builder =>
                 {
                     builder.AllowAnyOrigin();
