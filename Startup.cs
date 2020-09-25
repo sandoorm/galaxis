9b3ef4c2367ef492b6539fb52788beeb9b2250cb
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-25 21:12:08 +0200

    add cors

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace GalaxisProject_WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // localhost connection with the provided PostgreSQL docker-compose.yml
