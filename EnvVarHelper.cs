e7f38522b1af759217548347b47c2c4135710e4c
Sandor Maraczy <sandoorm@hotmail.com>
2020-09-28 21:39:26 +0200

    Revert "connnn str"
    This reverts commit f749f4b7d9eeab61d41ba72bf25f56e8c4f06be9.

--- EnvVarHelper.cs
+++ EnvVarHelper.cs
@@ -2,9 +2,9 @@
 {
     public static class EnvVarHelper
     {
-        // "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.50.48;Port=5432;Database=sampledb10;";
+        // "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.50.48;Port=5432;Database=sampledb2-galaxis;";
         // "User Id=galaxis;Password=galaxis;Server=localhost;Port=5432;Database=galaxis;";
         public static string GetGalaxisDbConnectionString() =>
-            "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.199.252;Port=5432;Database=sampledb10;";
+            "User Id=galaxis;Password=galaxis;Server=localhost;Port=5432;Database=galaxis;";
     }
 }
