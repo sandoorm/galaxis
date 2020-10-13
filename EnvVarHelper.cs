0668e657419d3c85d3b025d0769e8fc971a3fc80
Sandor Maraczy <sandoorm@hotmail.com>
2020-10-13 16:32:15 +0200

    moved to new openshift account

--- EnvVarHelper.cs
+++ EnvVarHelper.cs
@@ -5,6 +5,6 @@
         // "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.50.48;Port=5432;Database=sampledb2-galaxis;";
         // "User Id=galaxis;Password=galaxis;Server=localhost;Port=5432;Database=galaxis;";
         public static string GetGalaxisDbConnectionString() =>
-            "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.199.252;Port=5432;Database=sampledb100;";
+            "User Id=galaxis-user;Password=galaxis-pw;Server=172.30.192.143;Port=5432;Database=sampledb;";
     }
 }
